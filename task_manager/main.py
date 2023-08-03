8386b6e23bb5ef956900f6139a70a36e1619b46a
Jpcuomo <107761810+Jpcuomo@users.noreply.github.com>
2023-08-03 17:27:49 -0300

    Update main.py

--- task_manager/main.py
+++ task_manager/main.py
@@ -4,8 +4,10 @@ from task_manager import TasksManager
 default_tasks = ['update', 'delete', 'random', 'insert', 'select', 'browse']
 
 t_m1 = TasksManager(default_tasks)
-new_task = t_m1.prompt_task()
-aux_task = 'delete'
-
-t_m1.update_task(aux_task, new_task)
+t_m1.load_tasks_from_csv()
 t_m1.print_tasks()
+t_m1.replace_task()
+t_m1.add_task()
+t_m1.remove_task()
+t_m1.print_tasks()
+t_m1.save_modifications()
