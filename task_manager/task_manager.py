88f508ba9b2500ea06fb69d85e5c637068e7fc61
Jpcuomo <107761810+Jpcuomo@users.noreply.github.com>
2023-08-04 21:04:22 -0300

    Add files via upload

--- task_manager/task_manager.py
+++ task_manager/task_manager.py
@@ -1,4 +1,3 @@
-import re
 import csv
 
 
@@ -11,77 +10,36 @@ class TasksManager:
             self.tasks_list = list1
 
 
-    def prompt_new_task(self) -> str:
-        # Prompts the user for a new task
-        while True:
-            task = input('Input a new task (only letters or "_"): ')
-            if re.match(r'^[a-zA-Z_]+$', task):
-                return task.lower()
-            else:
-                print('Incorrect name for the task. Try again!')
-
-
-    def prompt_current_task(self) -> str:
-        # Prompts the user for a task
-        while True:
-            task = input('Input current task: ')
-            if re.match(r'^[a-zA-Z_]+$', task):
-                return task.lower()
-            else:
-                print('Incorrect name for the task. Try again!')
-
-
-    def print_tasks(self):
-        # Print tasks as an enumerated column
-        print('** TASKS **')
-        for i, task in enumerate(self.tasks_list, start=1):
-            print(f'{i}- {task.title()}')
-
-
-    def add_task(self):
+    def add_task(self, new_task):
         # Get a task as parameter and adds it to the list
-        new_task = self.prompt_new_task()
-        if new_task not in self.tasks_list:
-            self.tasks_list.append(new_task.lower())
-        else:
-            print('Task already exists')
+        self.tasks_list.append(new_task.lower())
+     
 
-
-    def remove_task(self):
+    def remove_task(self, current_task):
         # Remove the task passed as parameter
-        current_task = self.prompt_current_task()
-        if current_task in self.tasks_list:
-            self.tasks_list.remove(current_task)
-        else:
-            print('Task not in the list')
+        self.tasks_list.remove(current_task)
+        self.save_modifications(self.tasks_list)
 
 
-    def replace_task(self):
+    def replace_task(self, current_task, new_task):
         # Replace an existing task by a new one
-        current_task = self.prompt_current_task()
-        if current_task in self.tasks_list:
-            new_task = self.prompt_new_task()
-            task_index = self.tasks_list.index(current_task)
-            self.tasks_list[task_index] = new_task
-        else:
-            print('Task not in the list')
+        task_index = self.tasks_list.index(current_task)
+        self.tasks_list[task_index] = new_task
 
 
-    def save_modifications(self):
+    def save_modifications(self, tasks_list):
         # Save tasks in a CSV file
         with open('tasks.csv', 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(['Task'])  # Write the header
-            writer.writerows([[task] for task in self.tasks_list])
+            writer.writerows([[task] for task in tasks_list])
 
 
     def load_tasks_from_csv(self):
         # Load tasks list forma a CSV file
-        try:
-            with open('tasks.csv', 'r') as file:
-                reader = csv.reader(file)
-                next(reader)  # Omit the first row, which is the header
-                self.tasks_list = [row[0] for row in reader]
-        except FileNotFoundError:
-            # If file doesn't exist, no action is performed
-            pass
+        with open('tasks.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Omit the first row, which is the header
+            self.tasks_list = [row[0] for row in reader]
+            return self.tasks_list
+     
